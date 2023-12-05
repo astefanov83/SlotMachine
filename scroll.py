76d8c4ad71f2af457e9c79025d41ac2aa46a600f
astefanov83 <152632321+astefanov83@users.noreply.github.com>
2023-12-05 09:37:04 -0500

    Update scroll.py

--- scroll.py
+++ scroll.py
@@ -1,10 +1,30 @@
-import randomcolorgen
 import time
 import keyboard
+import random
 
 
 #action that lever is pulled
 lever = False
+
+def isUnique(arr):
+    test = arr[0]
+    for elem in range(1, len(arr)): 
+        if arr[elem] != test:
+            return False
+    return True
+
+def randcolorgen():
+
+    new_row = []
+
+    for elem in range(3):
+
+        color = random.randint(0,5)
+
+        new_row = new_row + [int(color)]
+
+    return new_row
+
 
 def scroll():
 
@@ -13,11 +33,10 @@ def scroll():
 
     lst = []
 
-    for i in range(30):
-        lst = lst + [randomcolorgen.randcolorgen()]
+    for i in range(1000):        
+        lst = lst + [randcolorgen()]
 
     i = 0
-
 
     while keyboard.is_pressed("space") == False: 
  
@@ -29,12 +48,16 @@ def scroll():
         time.sleep(0.1)
 
     print("FINAL: ", lst[i])
-
     
+    if isUnique(lst[i]) == True:
+        print("Game Won!")
+    if isUnique(lst[i]) == False:
+        print("Loser!")
+
+
 
 
 scroll()
-
 
     
 
